3f815b897da54d60bee6e1fee59c114439c245f0
Precios Ezeigbo <preciousezeigo81@gmail.com>
2025-04-27 16:11:46 +0100

    Fix: fixing port

--- app.py
+++ app.py
@@ -463,5 +463,5 @@ async def handle_github_webhook(
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
